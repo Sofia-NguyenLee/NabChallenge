fc9883f03b353a9a55a903577b2a72694e7ba0a7
LAPTOP-80VGVG01\NguyenLee <hoangnguyenletran@gmail.com>
2021-07-05 10:30:40 +0700

    Geolocation

--- cypress/integration/default_location.spec.js
+++ cypress/integration/default_location.spec.js
@@ -9,10 +9,34 @@ describe(specTitle('Convert temperature for default location'), function() {
         })
     }) 
 
-    it("Verify feature geolocation doesn't work", function() {
+    var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    };
+      
+    function success(pos) {
+        var crd = pos.coords;
+        console.log('Your current position is:');
+        console.log(`Latitude : ${crd.latitude}`);
+        console.log(`Longitude: ${crd.longitude}`);
+        console.log(`More or less ${crd.accuracy} meters.`);
+    }
+      
+    function error(err) {
+        cy.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    it("Verify current position", function() {
+        // Verify the geolocation is allowed        
+        navigator.permissions.query({name:'geolocation'}).then(function(result) {
+            // Will return ['granted', 'prompt', 'denied']
+            assert.equal(result.state, 'granted')            
+            navigator.geolocation.getCurrentPosition(success, error, options)
+        })
+        
         homePage.getForecastWeather().getDisplayedCity().then($element => {
-            cy.log('Allocated location: ' + $element.text())
-            assert.equal($element.text().indexOf('VN'), -1)
+            cy.log('Allocated location: ' + $element.text())            
         })
     })
 })
